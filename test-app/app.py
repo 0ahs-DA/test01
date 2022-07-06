a16951e798186972b9dbf5eec6da3e0436c075fa
0ahs-DA <50159866+0ahs-DA@users.noreply.github.com>
2022-07-07 02:13:50 +0900

    Update app.py

--- test-app/app.py
+++ test-app/app.py
@@ -152,5 +152,4 @@ def update_graph(option_slctd):
     
     
 if __name__ == '__main__':
-    app.run_server(port = 8050, dev_tools_ui=True, #debug=True,
-              dev_tools_hot_reload =True, threaded=True)
+    app.run_server(port = 8050)
